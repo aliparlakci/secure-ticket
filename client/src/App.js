197f614c899ac87224b1de1bc50b5afb25a83ed4
Ali Parlakçı <parlakciali@gmail.com>
2021-02-09 10:05:10 +0300

    Metamask working!

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { useContract } from "./contractProvider";
+import MetamaskProvider, { useMetamask } from "./metamaskProvider";
 
 function App() {
-  return <div></div>;
+  const account = useMetamask();
+  const contract = useContract();
+
+  const createEvent = () => {
+    contract.methods.createEvent("Ajda Pekkan", 1612850449).send({ from: account }).then(console.log);
+  }
+
+  return (
+    <div>
+      {account}
+      <button onClick={createEvent}>CreateEvent</button>
+    </div>
+  );
 }
 
 export default App;
