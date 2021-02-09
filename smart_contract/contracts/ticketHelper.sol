5e72eebd88f36279ff1b53028918dff930370098
Ali Parlakci <parlakciali@gmail.com>
2021-02-09 10:53:08 +0300

    getTicketsByOwner

--- smart_contract/contracts/ticketHelper.sol
+++ smart_contract/contracts/ticketHelper.sol
@@ -3,5 +3,15 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./ticketFactory.sol";
 
 contract TicketHelper is TicketFactory {
-    
+    function getTicketsByOwner(address _owner) external view returns(uint[] memory) {
+        uint[] memory result = new uint[](ownerTicketCount);   
+        uint counter;
+
+        for(uint i = 0; i < tickets.length; i++) {
+            if (ticketToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+    }
 }
